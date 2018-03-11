60ef88c8ac87665c65e419d084413410a65f85d7
WebDeveloperTom <tompowellgames@gmail.com>
2018-03-11 23:12:36 +1300

    added error handlers

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -13,7 +13,36 @@ if (isset($_POST['submit'])) {
     header("Location: ../signup.php?query=empty");
     exit();
   } else {
+    //check if input chars are valid.
+    if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last)) {
+      header("Location: ../signup.php?query=invalid");
+      exit();
+    } else {
+      //check if email is FILTER_VALIDATE_EMAIL
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        header("Location: ../signup.php?query=invalidemail&first=$first&last=$last&uid=$uid");
+        exit();
+      } else {
+        //checking if dulplicate email
+        $sql = "SELECT * FROM users WHERE user_uid = '$uid'";
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
 
+        if ($resultCheck > 0) {
+          header("Location: ../signup.php?query=usertaken");
+          exit();
+        } else {
+          // hashing $pwd
+          $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+          // Insert the user into the database
+          $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd)
+                  VALUES ('$first', '$last', '$email', '$uid', '$hashedPwd'); ";
+          mysqli_query($conn, $sql);
+          header("Location: ../signup.php?query=success");
+          exit();
+        }
+      }
+    }
   }
 
 
